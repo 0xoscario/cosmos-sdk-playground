e50a250431ad33d901ad64f0d2373725ccc87090
0xBlockchain <buckoscarengr@gmail.com>
2019-01-19 15:58:06 +0500

    struct created for keeper

---
+++ x/nameservice/keeper.go
@@ -0,0 +1,19 @@
+package nameservice
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// Keeper maintains the link to data storage and exposes getter/setter methods for the various parts of the state machine
+type Keeper struct {
+	coinKeeper bank.Keeper
+
+	namesStoreKey  sdk.StoreKey // Unexposed key to access name store from sdk.Context
+	ownersStoreKey sdk.StoreKey // Unexposed key to access owners store from sdk.Context
+	pricesStoreKey sdk.StoreKey // Unexposed key to access prices store from sdk.Context
+
+	cdc *codec.Codec // The wire codec for binary encoding/decoding.
+}
