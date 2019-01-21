e255d2f3ed11a3073c29afd09713863d580d755a
0xBlockchain <buckoscarengr@gmail.com>
2019-01-21 12:39:02 +0500

    resolver added for creating names and setting their values in msg

---
+++ x/nameservice/msgs.go
@@ -0,0 +1,24 @@
+package nameservice
+
+import (
+	"encoding/json"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// MsgSetName defines a SetName message
+type MsgSetName struct {
+	Name  string
+	Value string
+	Owner sdk.AccAddress
+}
+
+// NewMsgSetName is a constructor function for MsgSetName
+func NewMsgSetName(name string, value string, owner sdk.AccAddress) MsgSetName {
+	return MsgSetName{
+		Name:  name,
+		Value: value,
+		Owner: owner,
+	}
+}
+
