962cf248a1f1b277de998df266e3a7695f9ec840
0xBlockchain <buckoscarengr@gmail.com>
2019-01-21 17:19:30 +0500

    handler added for buying names and setting the price

--- x/nameservice/handler.go
+++ x/nameservice/handler.go
@@ -24,13 +24,13 @@ func NewHandler(keeper Keeper) sdk.Handler {
 		}
 	}
 }
+
 // NewHandler is essentially a sub-router
 // that directs messages coming into this module to the proper handler.
 
-
-// In this function, check to see if the Msg sender is actually the owner of the 
-// name (keeper.GetOwner). 
-// If so, they can set the name by calling the function on the Keeper. 
+// In this function, check to see if the Msg sender is actually the owner of the
+// name (keeper.GetOwner).
+// If so, they can set the name by calling the function on the Keeper.
 // If not, throw an error and return that to the user.
 
 func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) sdk.Result {
@@ -39,4 +39,29 @@ func handleMsgSetName(ctx sdk.Context, keeper Keeper, msg MsgSetName) sdk.Result
 	}
 	keeper.SetName(ctx, msg.Name, msg.Value)
 	return sdk.Result{}
+}
+
+func handleMsgBuyName(ctx sdk.Context, keeper Keeper, msg MsgBuyName) sdk.Result {
+	// Checks if the the bid price is greater than the price paid by the current owner
+	if keeper.GetPrice(ctx, msg.Name).isAllGT(msg.Bid) {
+		// If not, throw an error
+		return sdk.ErrInsufficientCoins("Bid not high enough").Result()
+	}
+	if keeper.HasOwner(ctx, msg.Name) {
+		_, err = keeper.coinKeeper.SendCoins(ctx, msg.Buyer, keeper.GetOwner(ctx, msg.Name), msg.Bid)
+
+		if err != nil {
+			return sdk.ErrInsufficientCoins("Buyer does not have enough coins").Result()
+		}
+	} else {
+		// if so, deduct the bid amount from the sender
+		_, _, err := keeper.coinKeeper.SubtractCoins(ctx, msg.Buyer, msg.Bid)
+		if err != nil {
+
+			return sdk.ErrInsufficientCoins("Buyer does not have enough coins").Result()
+		}
+	}
+	keeper.SetOwner(ctx, msg.Name, msg.Buyer)
+	keeper.SetPrice(ctx, msg.Name, msg.Buyer)
+	return sdk.Result()
 }
