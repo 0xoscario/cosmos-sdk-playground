ab7e2264256c36794629d521eaf8a77f9248cf7f
0xBlockchain <buckoscarengr@gmail.com>
2019-01-22 15:04:38 +0500

    command added for all query commands in client module

--- x/nameservice/client/cli/module_client.go
+++ x/nameservice/client/cli/module_client.go
@@ -17,4 +17,18 @@ func NewModuleClient(storekey string, cdc *amino.Codec) ModuleClient {
 	return ModuleClient(storekey, cdc)
 }
 
-//
+// GetQueryCmd - returns the cli commands for this module
+func (mc ModuleClient) GetQueryCmd() *cobra.Command  {
+
+	// Group gov queries under a subcommand
+	govQueryCmd = &cobra.Command {
+		Use: 	"nameservice",
+		Short: 	"Querying commands for the nameservice module",
+	}
+
+	govQueryCmd.AddCommand(client.GetCommands(
+		nameservicecmd.GetCmdResolveName(mc.storekey, mc.cdc),
+		nameservicecmd.GetCmdWhois(mc.storekey, mc.cdc)
+	)...)
+	return govQueryCmd
+}
