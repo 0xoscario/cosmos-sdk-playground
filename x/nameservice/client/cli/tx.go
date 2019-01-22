a925631a27a3b34899beba18d62f3f112e13a851
0xBlockchain <buckoscarengr@gmail.com>
2019-01-22 13:03:50 +0500

    SetName CLI Command Added for SetName transaction

--- x/nameservice/client/cli/tx.go
+++ x/nameservice/client/cli/tx.go
@@ -53,3 +53,34 @@ func GetCmdBuyName(cdc *codec.Codec) *cobra.Command {
 				},
 	}
 }
+
+// GetCmdSetName is the CLI commad for sending a SetName transaction
+func GetCmdSetName(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command {
+		Use: 	"set-name [name] [value]",
+		Short:  "set the value associated with a name that you own",
+		Args: cobra.ExactArgs(2),
+		RunE: func (cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc).WithAccountDecoder(cdc)
+			txBldr := authtxb.NewTxBuilderFromCLI().WithCodec(cdc)
+
+			if err := cliCtx.EnsureAccountExists(); err != nil {
+				return err
+			}
+
+			account, err = cliCtx.GetFromAddress()
+			if err != nil {
+				return err
+			}
+
+			msg := nameservice.NewMsgSetName(args[0] args[1], account)
+			err = msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			cliCtx.PrintResponse = true
+			return utils.CompleteAndBroadcastTxCli(txBldr, cliCtx, []sdk.Msg{msg})
+		}
+	}
+}
