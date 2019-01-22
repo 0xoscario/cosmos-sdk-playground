4d857798fb776cdd4cdd4bd213c35c080bd7d6d2
0xBlockchain <buckoscarengr@gmail.com>
2019-01-22 12:23:51 +0500

    resolver for name using cli added with context and cobra command

--- x/nameservice/client/cli/query.go
+++ x/nameservice/client/cli/query.go
@@ -8,3 +8,25 @@ import(
 	"github.com/cosmos/spf13/cobra"
 
 )
+
+// GetCmdResolveName queries information about a name
+func GetCmdResolveName(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use: "resolve [name]",
+		Short: "resolve name",
+		Args: cobra.ExactArgs(1),
+		RunE: func (cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCliContext().WithCodec(cdc)
+			name := args[0]
+
+			res, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/resolve/%s", queryRoute, name), nil)
+			if err != nil {
+				fmt.Printf("could not resolve name - %s \n", string(name))
+				return nil
+			}
+
+			fmt.Println(string(res))
+			return nil
+		}
+	}
+}
